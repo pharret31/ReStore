bb2312543906bf74e5020e1cdd9213cd452d4f5b
pharret31 <pharret31@gmail.com>
2022-07-05 21:02:57 +0300

    End of section 5

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,5 +1,7 @@
 using API.Data;
+using API.Middleware;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -37,9 +39,11 @@ namespace API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseMiddleware<ExceptionMiddleware>();
+
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                // app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
             }
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,15 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './app/layout/styles.css';
-import App from './app/layout/App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './app/layout/styles.css'
+import App from './app/layout/App'
+import reportWebVitals from './reportWebVitals'
+import { Router } from 'react-router-dom'
+import { createBrowserHistory } from 'history'
+
+export const history = createBrowserHistory();
 
 ReactDOM.render(
     <React.StrictMode>
-        <BrowserRouter>
+        <Router history={history}>
             <App />
-        </BrowserRouter>
+        </Router>
     </React.StrictMode>,
     document.getElementById('root')
 );
